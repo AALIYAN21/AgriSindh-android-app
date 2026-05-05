46cf59013b8c6487e514d8df61aa0d96ca4ee2cb
Kumail Habib <kumailhabib23@gmail.com>
2026-05-05 16:40:20 +0500

    EDITED HOME FOR CONFLICT RESOLUTION

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,11 +1,6 @@
-<<<<<<< Updated upstream
+
 import HomeScreen from "@/screens/HomeScreen";
 import React from "react";
-=======
-import HomeScreen from '@/screens/FeeCollectionScreen'
-import React from 'react'
-import { StyleSheet } from 'react-native'
->>>>>>> Stashed changes
 
 const home = () => {
   return <HomeScreen />;
